d5f47006bbbef481feeb616d1bfbd9b0a522286f
abjeet yadav <avjeetyadav00@gmail.com>
2026-08-20 18:05:00 +0530

    Time: 1 ms (97.63%), Space: 44.5 MB (40.4%) - LeetHub

--- 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.java
+++ 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.java
@@ -17,16 +17,16 @@ class Solution {
     int seenl = -1;
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
         List<List<Integer>> res = new LinkedList<>();
-        form(res,root,0);
+        form(res,root,1);
         return res;
     }
     private void form(List<List<Integer>> res,TreeNode root,int level){
         if(root==null)return;
-        if(level>seenl){
+        if(level>res.size()){
             seenl=level;
             res.addFirst(new ArrayList<>());
         }
-        res.get(seenl-level).add(root.val);
+        res.get(res.size()-level).add(root.val);
         form(res,root.left,level+1);
         form(res,root.right,level+1);
     }
